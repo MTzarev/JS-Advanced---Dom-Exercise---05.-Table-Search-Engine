c014b8a5e173b90e14d798979ad43cbafd8babe3
Miroslav Tsarev <mtzarev@gmail.com>
2024-04-30 20:42:57 +0300

    Add files via upload

---
+++ solution.js
@@ -0,0 +1,19 @@
+function solve() {
+   document.querySelector('#searchBtn').addEventListener('click', onClick);
+   let rows = document.querySelectorAll(`tbody tr`);
+
+
+   function onClick() {
+      let input = document.querySelector(`#searchField`).value.toLowerCase();
+
+      for (let row of rows) {
+         if (row.textContent.toLowerCase().includes(input)) {
+            row.setAttribute(`class`, `select`);
+         } else {
+            row.removeAttribute(`class`);
+
+         }
+      }
+
+   }
+}
