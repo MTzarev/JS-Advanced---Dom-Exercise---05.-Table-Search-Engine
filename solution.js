6177fbf39b6685e44046f2dc10564fdce3431674
Miroslav Tsarev <mtzarev@gmail.com>
2024-04-30 20:50:21 +0300

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,19 +1,14 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
    let rows = document.querySelectorAll(`tbody tr`);
-
-
-   function onClick() {
+function onClick() {
       let input = document.querySelector(`#searchField`).value.toLowerCase();
-
-      for (let row of rows) {
-         if (row.textContent.toLowerCase().includes(input)) {
+         for (let row of rows) {
+            if (row.textContent.toLowerCase().includes(input)) {
             row.setAttribute(`class`, `select`);
-         } else {
+            } else {
             row.removeAttribute(`class`);
-
          }
       }
-
    }
 }
